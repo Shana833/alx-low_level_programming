f0403c39ce7deceabbef95215a0eb21aced34f9b
Rokshana <rokshana833@gmail.com>
2023-02-16 13:47:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdioh>
+/*
+ *  main- Write a C Program using puts
+ * 
+ * Return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
