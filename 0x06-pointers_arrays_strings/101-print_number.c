350ed188920f49949aa2f0cf3ed2e4951317a8fb
Rokshana <rokshana833@gmail.com>
2023-03-03 03:08:24 -0800

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,22 +3,31 @@
 /**
  * print_number -  prints an integer
  * @n: integer to be printed
- * Return: 0
+ * Return: no return
  */
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+		_putchar(45);
+		m = n * -1;
 	}
-
-	if ((num / 10) > 0)
+	else
 	{
-		print_number(num / 10);
+		m = n;
 	}
-	putchar((num % 10) + '0');
+	d = m;
+	count = 1;
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+	_putchar(((m / count) % 10) + 48);
+	}
 }
