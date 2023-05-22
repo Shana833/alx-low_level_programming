f60294f45f70ca9b46b34b5c25bc9c2317c31342
Shana833 <rokshana833@gmail.com>
2023-05-22 10:42:38 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - write a C program with printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+} 
