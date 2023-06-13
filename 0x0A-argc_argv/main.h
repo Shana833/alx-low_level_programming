49a3f1c468c6e91d70d57544efc413fd6666f606
Shana833 <rokshana833@gmail.com>
2023-06-13 08:05:42 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int check_num(char *str);
 int _atoi(char *s);
 
 #endif
