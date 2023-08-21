7a12fc20e0f50dc2935052c50ec0c053a9c58e5b
Shana833 <rokshana833@gmail.com>
2023-08-21 09:20:21 -0700

    doubly_linked_lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 
 	if (head == NULL || *head == NULL)
-                return (-1);
+		return (-1);
 
 	current = *head;
 	if (index == 0)
